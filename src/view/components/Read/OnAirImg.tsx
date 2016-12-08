e8d722062f7639c9ab1372b26ddf5f801e07e11c
Minori.W <hunter_kcgr@yahoo.co.jp>
2016-12-08 19:34:54 +0900

    Read、ほぼ完成。

--- src/view/components/Read/OnAirImg.tsx
+++ src/view/components/Read/OnAirImg.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {Header} from "../../objects/Header";
 import {ReadMenu} from "./ReadMenu";
+import {Button} from "react-bootstrap";
 
 interface P {
 }
@@ -12,12 +13,17 @@ export class OnAirImg extends React.Component<P,S> {
   render(): React.ReactElement<any> {
     return (
       <div>
-        <div className="on-air-img">
+        <div className="read">
           <h2>きゅんonRadio</h2>
           <p>Now on Air</p>
-          <div className="read">
+          <div className="on-air-message">
             <p>PN:suneoさんからの投稿です。今日学校へ行ったら…(内容を表示)</p>
-            <img src="../images/on-air.gif" alt="on-air" />
+            <div className="read-select-btn">
+              <Button bsStyle="success">修正する</Button>
+              <Button bsStyle="info">Tweetする</Button>
+              <Button bsStyle="warning">ホームへ</Button>
+            </div>
+            <img src="../images/on-air.gif" alt="on-air"/>
           </div>
         </div>
       </div>
